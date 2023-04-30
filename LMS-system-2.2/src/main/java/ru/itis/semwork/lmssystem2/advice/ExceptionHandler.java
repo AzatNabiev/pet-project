65df951243edc24818e07cb7326d972a945dcdaf
AzatNabiev <azat.nabiev-azat@yandex.ru>
2023-04-30 15:39:16 +0200

    added dockerfile :Azat Nabiev

--- LMS-system-2.2/src/main/java/ru/itis/semwork/lmssystem2/advice/ExceptionHandler.java
+++ LMS-system-2.2/src/main/java/ru/itis/semwork/lmssystem2/advice/ExceptionHandler.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.itis.semwork.lmssystem2.dto.ExceptionDto;
 
-//@ControllerAdvice
-//public class ExceptionHandler extends ResponseEntityExceptionHandler {
-//    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-//    public ResponseEntity<ExceptionDto> handleError(Exception e) {
-//        return ResponseEntity.badRequest().body(ExceptionDto.builder().message(e.getLocalizedMessage()).build());
-//    }
-//}
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDto> handleError(Exception e) {
+        return ResponseEntity.badRequest().body(ExceptionDto.builder().message(e.getLocalizedMessage()).build());
+    }
+}
---
+++ LMS-system-2.2/src/main/java/ru/itis/semwork/lmssystem2/cors/WebCors.java
@@ -0,0 +1,22 @@
+package ru.itis.semwork.lmssystem2.cors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@EnableWebMvc
+@Configuration
+public class WebCors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL);
+    }
+}
